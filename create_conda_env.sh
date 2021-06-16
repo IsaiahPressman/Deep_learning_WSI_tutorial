cdb6fb3996c40de32425d831bded8d43e33e9833
Isaiah Pressman <isaiahpressman16@gmail.com>
2021-06-16 16:18:23 -0400

    Reworking env creation script

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -7,6 +7,6 @@ conda activate "$ENV_NAME"
 
 conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch -y
 conda install -c conda-forge notebook -y
-pip install tqdm
+conda install -c anaconda tqdm
 pip install openslide-python
 conda install -c anaconda pixman -y
