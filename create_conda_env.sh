e6b11a15e9f46f2f34c2fee8dd130edc33033c77
Isaiah Pressman <isaiahpressman16@gmail.com>
2021-06-16 16:02:20 -0400

    Reworking env creation script

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,3 +1,12 @@
-conda env create -f environment.yml
-conda activate Deep_learning_WSI_tutorial
+ENV_NAME="${1:-"Deep_learning_WSI_tutorial"}"
+CONDA_BASE=$(conda info --base)
+source "$CONDA_BASE"/etc/profile.d/conda.sh
+
+conda create -n "$ENV_NAME" python=3.7 -y
+conda activate "$ENV_NAME"
+
+conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch -y
+conda install -c conda-forge notebook -y
+pip install tqdm
 pip install openslide-python
+conda install -c anaconda pixman -y
