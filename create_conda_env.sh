82978a2fb74cdb09b2a17c32dd8f418aa47775ca
IsaiahPressman <isaiahpressman16@gmail.com>
2021-06-17 12:03:24 -0400

    Added library version requirements

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -6,7 +6,7 @@ conda create -n "$ENV_NAME" python=3.7 -y
 conda activate "$ENV_NAME"
 
 conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch -y
-conda install matplotlib pandas -y
-conda install -c conda-forge notebook -y
-pip install openslide-python
-conda install -c anaconda tqdm pixman==0.40.0 -y
+conda install matplotlib==3.* pandas==1.* -y
+conda install -c conda-forge notebook==6.* -y
+pip install openslide-python==1.1.1
+conda install -c anaconda tqdm==4.* pixman==0.40.0 -y
