bdd64d0b0f9fcf0400d03fd14850b933c4f70e0d
Isaiah Pressman <isaiahpressman16@gmail.com>
2021-06-23 13:57:21 -0400

    Added ipywidgets

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -10,6 +10,6 @@ conda activate "$ENV_NAME"
 # conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch -y
 conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.2 -c pytorch -y
 conda install matplotlib==3.* pandas==1.* -y
-conda install -c conda-forge notebook==6.* -y
+conda install -c conda-forge notebook==6.* ipywidgets==7.* -y
 pip install openslide-python==1.*
 conda install -c anaconda scipy==1.* scikit-learn==0.23.* tqdm==4.* pixman==0.40.0 -y
